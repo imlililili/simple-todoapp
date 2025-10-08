4e8e2567b66fedb5cdb4322aa233786030bab978
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2025-10-08 20:54:59 +1100

    add task description on addNewTask page, and updated Input w/ react-hook-form

--- app/add-task/page.tsx
+++ app/add-task/page.tsx
@@ -6,32 +6,53 @@ import { v4 as uuidv4 } from "uuid";
 import { addTodo } from "@/api";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
+import { ITask } from "@/types/tasks";
+
+interface TaskProps {
+    task: ITask
+}
 
 export default function AddTaskPage() {
+    const { register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+      } = useForm<ITask>();
+
     const router = useRouter();
-    const [newTaskValue, setNewTaskValue] = useState<string>("");
+    // const [newTaskValue, setNewTaskValue] = useState<string>("");
 
-    const handleSubmitNewTodo = async (e) => {
+    // const handleSubmitNewTodo = async (e) => {
         
-        e.preventDefault();
-        if (!newTaskValue.trim()) { // Prevent adding empty tasks
-            alert("Please enter a task title!");
-            return;
-        }
-        await addTodo({
-            id: uuidv4(),
-            text: newTaskValue
-        });
-        setNewTaskValue("");
-        router.push("/");
-    };
+    //     e.preventDefault();
+    //     if (!newTaskValue.trim()) { // Prevent adding empty tasks
+    //         alert("Please enter a task title!");
+    //         return;
+    //     }
+    //     await addTodo({
+    //         id: uuidv4(),
+    //         text: newTaskValue
+    //     });
+    //     setNewTaskValue("");
+    //     router.push("/");
+    // };
 
+
+    const onSubmit = async (data: FieldValues) => {
+        await addTodo({   
+            id: uuidv4(),
+            text: data.task,
+            description: data.description
+        });
+        router.push("/");
+    }
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-4">
             <h1 className="text-2xl font-bold mb-6">Add New Task</h1>
 
-            <form onSubmit={handleSubmitNewTodo} className="flex flex-col gap-4 w-80">
-                <Input type="text" placeholder="Task" value={newTaskValue} onChange={(e)=> setNewTaskValue(e.target.value)} />
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-80">
+                <Input {...register("task", {required: "Task title is required"})} type="text" placeholder="Task"  />
+                <Input {...register("description")} type="text" placeholder="Description"  />
                 <Button type="submit" variant="outline">
                     Save
                 </Button>
