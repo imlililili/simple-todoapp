3b8ce8b0aa8f12dee5858a1524028681462ab8be
LiYing Chen <78844463+imlililili@users.noreply.github.com>
2025-11-07 22:38:57 +1100

    state ???

---
+++ app/store/todoStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+const useTodoStore = create(set => ({
+    todos: [],
+    addTodo: (text) =>set((state) => ({todos: [...state.todos, {text, completed:false, id: string}]})),
+    removeTodo: (id) =>set((state) => ({todos: state.todos.filter(t => t.id != id)})),
+    toggleTodo: (id) =>set((state) => ({todos: state.todos.map(t => t.id === id? {...t, completed: !t.completed}: t)})),
+}))
+
+export default useTodoStore;
